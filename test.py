c484a91187ae0a9342584da7bc5c23bb3eefd8a4
sanja_s <sanja100v@mail.ru>
2020-05-22 17:28:21 +0300

    remove all comments

--- test.py
+++ test.py
@@ -1,20 +1,6 @@
-import time
 import webbrowser
-import sys
 
-# import pandas
-
-
-# print(sys.argv)
-
-
-# l = [1, 2 , 3 , 4]
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyey2UzoVlI8AWtPja1kwtIHatRPO' \
       '-W2dGAb3LwuppYuJ1ZNjDfmLQGJy57dVUdVpsDrHHf9_Z1W5t/pubhtml# '
 
 webbrowser.open(url)
-
-# import datetime
-
-
-# datetime.time()
