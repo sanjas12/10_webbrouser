7d4e01996093438990f08502569204a05829ead1
stovpyaga <sanja100v@gmail.com>
2020-05-22 15:30:12 +0300

    first commit

---
+++ test.bat
@@ -0,0 +1,9 @@
+@echo off
+date /t
+echo 12
+%time%
+
+@python d:\sanja\My_Work\Python\10_webbrouser\test.py %*
+Rem comment
+Rem @pause
+
---
+++ test.py
@@ -0,0 +1,22 @@
+import time
+import webbrowser
+import sys
+# import pandas
+
+
+print(sys.argv)
+
+
+# l = [1, 2 , 3 , 4]
+
+
+webbrowser.open(
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyey2UzoVlI8AWtPja1kwtIHatRPO'
+        '-W2dGAb3LwuppYuJ1ZNjDfmLQGJy57dVUdVpsDrHHf9_Z1W5t/pubhtml#')
+
+# import datetime
+
+
+# datetime.time()
+
+
