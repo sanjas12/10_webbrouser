bbcd21f35d687b334b9fa362b9754cca51a2cc9b
sanja_s <sanja100v@mail.ru>
2020-05-22 17:23:26 +0300

    add variable "url"

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import time
 import webbrowser
 import sys
+
 # import pandas
 
 
@@ -8,15 +9,12 @@ import sys
 
 
 # l = [1, 2 , 3 , 4]
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyey2UzoVlI8AWtPja1kwtIHatRPO' \
+      '-W2dGAb3LwuppYuJ1ZNjDfmLQGJy57dVUdVpsDrHHf9_Z1W5t/pubhtml# '
 
-
-webbrowser.open(
-        'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyey2UzoVlI8AWtPja1kwtIHatRPO'
-        '-W2dGAb3LwuppYuJ1ZNjDfmLQGJy57dVUdVpsDrHHf9_Z1W5t/pubhtml#')
+webbrowser.open(url)
 
 # import datetime
 
 
 # datetime.time()
-
-
