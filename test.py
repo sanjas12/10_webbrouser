680878bfd1a29a8a291094dd81b8bc654971484c
sanja_s <sanja100v@mail.ru>
2020-05-24 21:59:53 +0300

    test lenovo

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyey2UzoVlI8AWtPja1kwtIH
       '-W2dGAb3LwuppYuJ1ZNjDfmLQGJy57dVUdVpsDrHHf9_Z1W5t/pubhtml# '
 
 webbrowser.open(url)
+
+# comment from my descto
